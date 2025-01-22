0ed648281093146bbe3526652544c2e65f3b1fde
Thomas Skerbis <thomas.skerbis@mac.com>
2025-01-22 10:15:50 +0100

    Update index.php

--- pages/index.php
+++ pages/index.php
@@ -5,7 +5,7 @@
  * @author markus[dot]staab[at]redaxo[dot]de Markus Staab
  */
 
-$addon = rex_addon::get('phpmailer');
+$addon = rex_addon::get('symfony_mailer');
 
 echo rex_view::title($addon->i18n('title'));
 rex_be_controller::includeCurrentPageSubPath();
